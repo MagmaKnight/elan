6c47b0bad675e47a544c07a6bf40f58f65ad5fc8
$(MagmaKnight) <$(MagmaKnight)@users.noreply.github.com>
2020-08-07 18:26:15 -0300

    Updates

---
+++ precache-manifest.3460b9e06f6aa18c4f47443dd06bd5ad.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae1c7e0313c3f80e6722d54dd5b6a2ee",
+    "url": "/elan/index.html"
+  },
+  {
+    "revision": "88eeccee2db2c0da149e",
+    "url": "/elan/static/css/main.73c63b79.chunk.css"
+  },
+  {
+    "revision": "921935ed14b8a6918cb6",
+    "url": "/elan/static/js/2.284e16aa.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/elan/static/js/2.284e16aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88eeccee2db2c0da149e",
+    "url": "/elan/static/js/main.0d910e3c.chunk.js"
+  },
+  {
+    "revision": "614fe305886227641297",
+    "url": "/elan/static/js/runtime-main.3c907906.js"
+  },
+  {
+    "revision": "8917856b57da55472606a59f43b41588",
+    "url": "/elan/static/media/CenturyGothic.8917856b.ttf"
+  },
+  {
+    "revision": "862c83306ca682ad1bbc1e66410f108e",
+    "url": "/elan/static/media/Cheddar.862c8330.ttf"
+  },
+  {
+    "revision": "b7ee872a66f976ca04e5ead63186b106",
+    "url": "/elan/static/media/acupuntura1.b7ee872a.jpg"
+  },
+  {
+    "revision": "7c1770da4d28241ba46a3b935f9b4ca2",
+    "url": "/elan/static/media/acupuntura2.7c1770da.jpg"
+  },
+  {
+    "revision": "919c8a7f1f1f9fd7abffc49151072705",
+    "url": "/elan/static/media/aline.919c8a7f.jpg"
+  },
+  {
+    "revision": "52c40deb9540b4afdc116304c41f0b3e",
+    "url": "/elan/static/media/arthur.52c40deb.jpg"
+  },
+  {
+    "revision": "5b23a837268b09d0efa9e307023c1dae",
+    "url": "/elan/static/media/instagram.5b23a837.svg"
+  },
+  {
+    "revision": "ad0354c87e18676d77b5186ef7f69747",
+    "url": "/elan/static/media/itau.ad0354c8.png"
+  },
+  {
+    "revision": "9da0b09013f2b0a5403031236846f96e",
+    "url": "/elan/static/media/logo.9da0b090.jpeg"
+  },
+  {
+    "revision": "e6a6699bf660ece3717d8bf21a1d2527",
+    "url": "/elan/static/media/massagem1.e6a6699b.jpg"
+  },
+  {
+    "revision": "81a21ea95009764ef9a3434e2e6dd276",
+    "url": "/elan/static/media/massagem2.81a21ea9.jpg"
+  },
+  {
+    "revision": "6a6087902c1670b5c75ff4ce2afb460d",
+    "url": "/elan/static/media/mercadopago.6a608790.png"
+  },
+  {
+    "revision": "e9575897eff59dee011645e07b4feef7",
+    "url": "/elan/static/media/moxabust.e9575897.jpg"
+  },
+  {
+    "revision": "eed12bd4a2163d008d4d51392003916c",
+    "url": "/elan/static/media/moxabust2.eed12bd4.jpg"
+  },
+  {
+    "revision": "fe7758a8a43e9ec16b75ea187c93b1f0",
+    "url": "/elan/static/media/moxabust3.fe7758a8.jpg"
+  },
+  {
+    "revision": "6207ea70a77b05ba6fc5881ccaaf670f",
+    "url": "/elan/static/media/moxabust4.6207ea70.jpg"
+  },
+  {
+    "revision": "24f6d89bfd8e4d50bb57331efa2a6416",
+    "url": "/elan/static/media/nubank.24f6d89b.png"
+  },
+  {
+    "revision": "36cf743d990ed98fc6b2afacb9fdff6f",
+    "url": "/elan/static/media/picpay.36cf743d.png"
+  },
+  {
+    "revision": "10ad3de14fe836713188d409cbb4e692",
+    "url": "/elan/static/media/produto1.10ad3de1.jpg"
+  },
+  {
+    "revision": "67e1545a9984056e7e13b3d027ee0418",
+    "url": "/elan/static/media/produto2.67e1545a.jpg"
+  },
+  {
+    "revision": "b53c44ade1df4039f1390274ee88f194",
+    "url": "/elan/static/media/whatsapp.b53c44ad.svg"
+  }
+]);
